07ec66f4857681e59ff4580b22d70291b8589448
D2L <ddlgamers14@gmail.com>
2021-01-26 18:26:22 +0700

    test lagi

---
+++ input.py
@@ -0,0 +1,17 @@
+nama = input("Tulisakan Nama : ")
+umur = input("Umur : ")
+status = input("siswa/lulus : ")
+if (status == "siswa"):
+    sekolah = input ("Asal Sekolah : ")
+else:
+    lulus = input ("Pekerjaan : ")
+
+print ("Nama : ",nama)
+print ("Umur : ",umur)
+if (status == "siswa"):
+    print ("Asal Sekolah : ",sekolah)
+else:
+    print ("Pekerjaan : ",lulus)
+
+
+
